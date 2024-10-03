7bee88d0f5d9129c35e056904c19b5fa5fdead53
Joy <oxumlabs@proton.me>
2024-10-03 19:58:20 +0530

    updated bins

--- sam/build/sam.c
+++ sam/build/sam.c
@@ -1,37 +1,45 @@
 #include <stdio.h>
-int fdi(int a, int b) {
-    if (b == 0) {
+int fdi(int a, int b)
+{
+    if (b == 0)
+    {
         // Handle error (e.g., return 0 or some error code)
         return 0;
     }
     int result = a / b;
     // Adjust result if a and b have different signs and there's a remainder
-    if ((a % b != 0) && ((a < 0) != (b < 0))) {
+    if ((a % b != 0) && ((a < 0) != (b < 0)))
+    {
         result--;
     }
     return result;
 }
 
-double fdf(double a, double b) {
-    
-    if (b == 0.0) {
+double fdf(double a, double b)
+{
+
+    if (b == 0.0)
+    {
         return 0.0;
     }
-    
+
     double result = a / b;
-    if (result > 0 && result != (int)result) {
+    if (result > 0 && result != (int)result)
+    {
         return (int)result; // Truncates towards zero
     }
-    if (result < 0 && result != (int)result) {
+    if (result < 0 && result != (int)result)
+    {
         return (int)result - 1;
     }
-    
+
     return result;
 }
 
-int main() {
+int main()
+{
     char z[1025] = "joy";
-    fgets(z,1025,stdin);
+    fgets(z, 1025, stdin);
     printf("%s\n", z);
     return 0;
 }
